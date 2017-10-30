eb0efa6326f87c873e21e3b098ca07d1e0888853
Maximilian Toller <maximilian.toller@student.tugraz.at>
2017-10-30 21:28:35 +0100

    refactoring and harmonic analysis

--- r/trueSeasonLength.R
+++ r/trueSeasonLength.R
@@ -24,6 +24,55 @@ modusCount = function(x)
   return(max(tabulate(match(x,unique(x)))));
 }
 
+harmonic = function(n)
+{
+  return(cumsum(1/c(1:n)));
+}
+
+aze = function(y, harmon = FALSE, zero_discard=FALSE,skew_correction=FALSE)
+{
+  n = nrow(y);
+  na = n-1;
+  
+  a = acf.fft(y);
+  a = a[2:length(a)];
+  a = a[1:round(length(a)*2/3)];
+  
+  if (harmon == TRUE)
+  {
+    a = a * (1/harmonic(round(na*2/3)));
+  }
+  
+  signs = a;
+  signs[which(signs < 0)] = -1;
+  signs[which(signs > 0)] = 1;
+  
+  delta = which(signs[2:na] + signs[1:(na-1)] == 0);
+  eta = a[delta] / (-1*(a[delta+1]-a[delta]));
+  zeros = delta+eta;
+  z_to_z = sort(zeros[2:na]-zeros[1:(na-1)]);
+  
+  if (zero_discard == TRUE)
+  {
+    z_to_z = z_to_z[which(z_to_z >= 3)];
+  }
+  
+  d = density(z_to_z,kernel = 'epanechnikov')
+  if (skew_correction && skewness(d$y) > 0)
+  {
+    d$y = d$y * c(1:round(length(d$y)))/sum(1:length(d$y))*length(d$y)#*skewness(d$y)*IQR(d$x) * length(d$y)/10;
+  }
+  zero_matrix = matrix(0,length(z_to_z),2);
+  zero_matrix[,1] = z_to_z;
+  par(mfrow=c(2,2));
+  plot(y);
+  plot(a,type = 'l');
+  
+  plot(zero_matrix);
+  plot(d);
+  
+  return(d);
+}
 
 trueSeasonLength = function(y){
   library(signal);
@@ -41,26 +90,19 @@ trueSeasonLength = function(y){
     print('y already has a frequency');
     return(frequency(y));
   }
+  
   n = nrow(y);
   na = n-1;
-  #y = detrend(y);
+  y = detrend(y);
   y = scale(y);
-  a = acf.fft(y);
+  #b = butter(2,0.1);
+  #y = filter(b$b,b$a,y);
+  dens = aze(y);
+  s = round(dens$x[which.max(dens$y)]*2);
   
-  a = a[2:length(a)];
   
-  signs = a;
   
-  signs[which(signs < 0)] = -1;
-  signs[which(signs > 0)] = 1;
-  
-  delta = which(signs[2:na] + signs[1:(na-1)] == 0);
-  eta = a[delta] / (-1*(a[delta+1]-a[delta]));
-  zeros = delta+eta;
-  z_to_z = sort(zeros[2:na]-zeros[1:(na-1)]);
-  m = matrix(0,length(z_to_z),2);
-  m[,1] = z_to_z;
-  
+  #-------------------------------------
   #ma = forecast::ma(a,s,centre = FALSE)
   #entropy and compression
   #kalman
@@ -68,30 +110,30 @@ trueSeasonLength = function(y){
   #backproject with shapelet stauchen
   #implement old algorithm in R
   #anisotropic diffusion
-  par(mfrow=c(2,2));
-  plot(y);
-  #plot(a,type = 'l');
-  hist(a,c(-1.0,-0.9,-0.8,-0.7,-0.6,-0.5,-0.4,-0.3,-0.2,-0.1,0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.0),freq = FALSE)
-  print(mean(a));
-  print(std(a));
-  print(skewness(a));
-  print(excess(a));
+  
+  #hist(a,c(-1.0,-0.9,-0.8,-0.7,-0.6,-0.5,-0.4,-0.3,-0.2,-0.1,0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.0),freq = FALSE)
+
+  #print(mean(a));
+  #print(std(a));
+  #print(skewness(a));
+  #print(excess(a));
+  
   #hist(y,c(-3.0,-2.0,-1.0,-0.9,-0.8,-0.7,-0.6,-0.5,-0.4,-0.3,-0.2,-0.1,0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.0,2.0,3.0));
   #ma = as.ts(fastMa(a,round(s/2)));
-  plot(m);
-  #print(z_to_z);
-  d = density(m[,1],kernel = 'epanechnikov')
-  plot(d);
+  #if (skewness(d$y) > 0)
+  #{
+  #  print(d$x[which.max(d$y)]-d$x[which.max(old_dy)]);
+  #  print(max(abs(d$y-old_dy)));
+  #  print(d$x[which.max(abs(d$y-old_dy))]);
+  #}
+  
   #s = 2*(round(median(z_to_z)));
   #print(d);
-  print(which.max(d$y));
-  print(d$x);
-  s = round(d$x[which.max(d$y)]*2);
-  if (is.na(s))
-  {
-    s = round(n/10);
-  }
-  print(d$y*100);
+  #print(which.max(d$y));
+  #print(sum(d$y));
+  #print(d$x);
+
+  #print(d$y*100);
   #interval_borders = unique(round((which(diff(sign(diff(d$y)))==-2)+1)/512*nrow(m)));
   #print(interval_borders);
   #print(z_to_z[interval_borders])
@@ -135,8 +177,6 @@ trueSeasonLength = function(y){
   #print("noise partition");
   #print(noise_partition);
   
-  ##
-  #plot(a);
-  print('end');
+  #-----------------------------
   return(s);
 }
