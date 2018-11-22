dae4f76cdc98a62be9cb831234cfb38bfd07d4a1
Max Toller <mtoller@know-center.at>
2018-11-22 13:16:12 +0100

    added paste0

--- r/callSeasonLength.R
+++ r/callSeasonLength.R
@@ -20,7 +20,7 @@ callSeasonLength = function(y)
   write(y,file = paste0('..',pathSymbol,'matlab',pathSymbol,'temp'),ncolumns = 1);
   system(paste0('cd ..',pathSymbol,'matlab;octave --silent --no-gui --eval "callSeasonLength()"'));
   system(paste0('cd ..',pathSymbol,'r'));
-  result = as.numeric(read.table('..',pathSymbol,'matlab',pathSymbol,'temp')[1])
+  result = as.numeric(read.table(paste0('..',pathSymbol,'matlab',pathSymbol,'temp'))[1])
   if (result == 0)
   {
     result = 1;
